e346c03d7ef5f21f2d5a9a84a1b170f4f95a7136
Chetna Joshi <chetnajoshi889@gmail.com>
2021-11-15 18:39:02 +0530

    completed job portal app

--- src/main/java/com/dc/jobPortal/dto/JobPortalSearchDTO.java
+++ src/main/java/com/dc/jobPortal/dto/JobPortalSearchDTO.java
@@ -2,10 +2,14 @@ package com.dc.jobPortal.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class JobPortalSearchDTO {
 
+    @NotBlank(message = "Please provide skill.")
     private String skill;
 
+    @NotBlank(message = "Please provide location.")
     private String location;
 }
